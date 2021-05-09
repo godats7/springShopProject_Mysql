74f92698116e0f878aa2d7d549925032c693f84c
LWJ <godats7@naver.com>
2021-05-09 23:25:04 +0900

    202105509

--- src/main/java/com/lwj/controller/AdminController.java
+++ src/main/java/com/lwj/controller/AdminController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -255,6 +256,33 @@ public class AdminController {
 		
 		return "redirect:/admin/authorManage";
 		
+	}
+	
+	/* 첨부 파일 업로드 */
+	@PostMapping("/uploadAjaxAction")
+	public void uploadAjaxActionPost(MultipartFile[] uploadFile) {
+		
+		logger.info("uploadAjaxActionPost..........");
+		
+//		logger.info("파일 이름 : " + uploadFile.getOriginalFilename());
+//		logger.info("파일 타입 : " + uploadFile.getContentType());
+//		logger.info("파일 크기 : " + uploadFile.getSize());
+
+		// 향상된 for
+				for(MultipartFile multipartFile : uploadFile) {
+					logger.info("-----------------------------------------------");
+					logger.info("파일 이름 : " + multipartFile.getOriginalFilename());
+					logger.info("파일 타입 : " + multipartFile.getContentType());
+					logger.info("파일 크기 : " + multipartFile.getSize());			
+				}
+				
+				//기본 for
+				for(int i = 0; i < uploadFile.length; i++) {
+					logger.info("-----------------------------------------------");
+					logger.info("파일 이름 : " + uploadFile[i].getOriginalFilename());
+					logger.info("파일 타입 : " + uploadFile[i].getContentType());
+					logger.info("파일 크기 : " + uploadFile[i].getSize());			
+				}
 	}
 	
 	
