054cfcf3b357c363daa0b946aaac2f54286cc81d
LWJ <godats7@naver.com>
2021-06-01 20:41:54 +0900

    20210601

--- src/main/java/com/lwj/controller/ImageController.java
+++ src/main/java/com/lwj/controller/ImageController.java
@@ -1,10 +1,19 @@
 package com.lwj.controller;
 
 
+import java.io.File;
+import java.io.IOException;
+
+import java.nio.file.Files;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,10 +33,35 @@ public class ImageController {
 		System.out.println("main나간다");
 		
 		return "main";
-	
 		
 	
 }
+	
+	@GetMapping("/display")
+	public ResponseEntity<byte[]> getImage(String fileName){
+		
+		logger.info("getImage().........."+fileName);
+		
+		File file = new File("c:\\upload\\" + fileName);
+		
+		ResponseEntity<byte[]> result = null;
+		
+		try {
+			
+			HttpHeaders header = new HttpHeaders();
+			
+			header.add("Content-type", Files.probeContentType(file.toPath()));
+			
+			result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+			
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return result;
+		
+	}
+	
 /* 아래의 방법이 안되서 리턴으로 함  (org.springframework.web.servlet.pagenotfound - no mapping for get) */
 
 //@Controller
--- src/main/webapp/WEB-INF/views/admin/goodsEnroll.jsp
+++ src/main/webapp/WEB-INF/views/admin/goodsEnroll.jsp
@@ -149,6 +149,12 @@
                     			</div>
                     			<div class="form_section_content">
 									<input type="file" multiple id ="fileItem" name='uploadFile' style="height: 30px;">
+									<div id="uploadResult">
+									<!-- 	<div id="result_card">
+											<div class="imgDeleteBtn">x</div>
+											<img src="/resources/img/Logo.png">
+										</div> -->
+									</div>
                     			</div>
                     		</div> 
                    		</form>
@@ -590,6 +596,7 @@ ClassicEditor
 	    	dataType : 'json',
 	    	success : function(result){
 	    		console.log(result);
+	    		showUploadImage(result);
 	    	},
 	    	error : function(result){
 	    		alert("이미지 파일이 아닙니다.");
@@ -616,6 +623,29 @@ ClassicEditor
 		
 		
 		return true;		
+		
+	}
+	
+	/* 이미지 출력 */
+	function showUploadImage(uploadResultArr){
+		
+		/* 전달받은 데이터 검증 */
+		if(!uploadResultArr || uploadResultArr.length == 0){return}
+		
+		let uploadResult = $("#uploadResult");
+		
+		let obj = uploadResultArr[0];
+		
+		let str = "";
+		
+		let fileCallPath = encodeURIComponent(obj.uploadPath.replace(/\\/g, '/') + "/s_" + obj.uuid + "_" + obj.fileName);
+		
+		str += "<div id='result_card'>";
+		str += "<img src='/display?fileName=" + fileCallPath +"'>";
+		str += "<div class='imgDeleteBtn'>x</div>";
+		str += "</div>";		
+		
+   		uploadResult.append(str); 
 		
 	}
 
